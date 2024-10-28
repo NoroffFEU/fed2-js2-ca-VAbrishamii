955dcc98fb55144dfdcdd79ea3eadb65835391b3
VAbrishamii <vahide.abri@yahoo.com>
2024-10-28 10:24:13 +0100

    set up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./**/*.{html,js,ts}","!./node_modules/**/*"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
