1d1324ec24424138d1ef5f8794ad7c95fb9b58d7
VAbrishamii <vahide.abri@yahoo.com>
2024-10-29 20:58:54 +0100

    Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,25 @@ export default {
     "./**/*.{html,js,ts}","!./node_modules/**/*"
   ],
   theme: {
+    extend: {
+      colors:{
+        primary: '#d90429',
+        secondary: {
+          light:'#ff5a5f',
+          dark:'#ef233c'
+        },
+        background:{
+          light:'#F0F2F5',
+          dark:'#18191A'
+        },
+        text:{
+          light:'#E3E4E8',
+          dark:'#45494F'
+        }
+        
 
-    extend: {},
+      }
+    },
   },
   plugins: [],
 }
